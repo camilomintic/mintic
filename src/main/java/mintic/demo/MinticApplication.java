ba88174221901ff1bfb78d2bbb0c35f598b7d06e
juancamilo <juan.abella.mt@correo.usa.edu.co>
2021-11-14 09:04:02 -0500

    Primera version

---
+++ src/main/java/mintic/demo/MinticApplication.java
@@ -0,0 +1,13 @@
+package mintic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic/demo/MinticApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
